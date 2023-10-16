b882000b463aa212c2b8dbba43c01eff645c3bad
laplacelarper <109858056+laplacelarper@users.noreply.github.com>
2023-10-16 16:29:49 +0000

    added dynamic changes to the file

--- APPE.py
+++ APPE.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+JOBS = [
+    {
+        'location': 'india',
+        'title': "seo",
+        'company': "google",
+        'description':'sssss'
+    },
+    {
+        'location': 'us',
+        'title': "leo",
+        'company': "facebook",
+        'description':'sssss'
+    },
+    {
+        'location': 'eu',
+        'title': "seraaa",
+        'company': "netflix",
+        'description':'sssss'
+    }
+]
 
 @app.route('/')
 def hello_world():
-    return 'Hello, urjtiam!'
-
+    return render_template('home.html',jobs=JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__=='__main__':
+  
   app.run(host='0.0.0.0', debug=True)
