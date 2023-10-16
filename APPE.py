0c01cd0e6b068ba3ba173e7c6b1c5bffe41a5484
laplacelarper <109858056+laplacelarper@users.noreply.github.com>
2023-10-16 17:56:38 +0000

    everything changed when heroku nation attacked...

--- APPE.py
+++ APPE.py
@@ -1,33 +1,42 @@
-from flask import Flask, jsonify, render_template
+  from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
-JOBS = [
+  app = Flask(__name__)
+
+  JOBS = [
     {
-        'location': 'india',
-        'title': "seo",
-        'company': "google",
-        'description':'sssss'
+      'id': 1,
+      'title': 'Data Analyst',
+      'location': 'Bengaluru, India',
+      'salary': 'Rs. 10,00,000'
     },
     {
-        'location': 'us',
-        'title': "leo",
-        'company': "facebook",
-        'description':'sssss'
+      'id': 2,
+      'title': 'Data Scientist',
+      'location': 'Delhi, India',
+      'salary': 'Rs. 15,00,000'
     },
     {
-        'location': 'eu',
-        'title': "seraaa",
-        'company': "netflix",
-        'description':'sssss'
+      'id': 3,
+      'title': 'Frontend Engineer',
+      'location': 'Remote'
+    },
+    {
+      'id': 4,
+      'title': 'Backend Engineer',
+      'location': 'San Francisco, USA',
+      'salary': '$150,000'
     }
-]
+  ]
 
-@app.route('/')
-def hello_world():
-    return render_template('home.html',jobs=JOBS)
-@app.route("/api/jobs")
-def list_jobs():
-  return jsonify(JOBS)
-if __name__=='__main__':
-  
-  app.run(host='0.0.0.0', debug=True)
+  @app.route("/")
+  def hello_jovian():
+      return render_template('home.html', 
+                             jobs=JOBS, 
+                             company_name='Jovian')
+
+  @app.route("/api/jobs")
+  def list_jobs():
+    return jsonify(JOBS)
+
+  if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
